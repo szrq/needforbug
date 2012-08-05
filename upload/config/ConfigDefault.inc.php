b5647864298759e9253afd24d934e1411da316d5
doyouhaobaby liu <6357505556@qq.com>
2012-08-05 20:09:57 +0800

    更改编码为正确的UTF-8

--- upload/config/ConfigDefault.inc.php
+++ upload/config/ConfigDefault.inc.php
@@ -5,7 +5,7 @@
 !defined('DYHB_PATH') && exit;
 
 return array(
-	// ���ݿ����
+	// 数据库相关
 	'DB_PASSWORD'=>'123456',
 	'DB_PREFIX'=>'needforbug_',
 	'DB_NAME'=>'needforbug',
@@ -13,10 +13,10 @@ return array(
 	'DB_CACHE'=>TRUE,
 	'DB_CACHE_TIME'=>86400000000,
 
-	// ϵͳ����
+	// 系统调试
 	'APP_DEBUG'=>TRUE,
 
-	// ��Ҫǰ׺
+	// 重要前缀
 	'RBAC_DATA_PREFIX'=>'rbac_data_prefix_needforbug_',
 	'COOKIE_PREFIX'=>'needforbug_',
 
@@ -41,12 +41,12 @@ return array(
 	'GUEST_AUTH_ID'=>0,
 	'RBAC_ERROR_PAGE'=>'',
 
-	// ʱ��
+	// 时区
 	'TIME_ZONE'=>'Asia/Shanghai',
 	
-	// ����ע�Ͱ�ģ���ǩ���
+	// 开启注释版模板标签风格
 	'TEMPLATE_TAG_NOTE'=>true,
 
-	// ����������
-	'APP_DEVELOP'=>0,// �Ƿ�����̨Ӧ����ƣ���Ӧ�ÿ���������Ϊ1
+	// 开发者中心
+	'APP_DEVELOP'=>0,// 是否开启后台应用设计，仅应用开发者设置为1
 );
