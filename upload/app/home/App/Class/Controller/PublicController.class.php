431628dd2f0723ca91b6d4c0d366df8b3ce550df
weiyongming <kevin394578420@gmail.com>
2012-08-22 16:33:18 +0800

    增加找回密码功能（通过email）

--- upload/app/home/App/Class/Controller/PublicController.class.php
+++ upload/app/home/App/Class/Controller/PublicController.class.php
@@ -205,6 +205,120 @@ class PublicController extends InitController{
 		}
 	}
 	
+	public function get_password(){
+		if(UserModel::M()->isLogin()){
+			$this->U('home://user/password');
+		}
+	
+		$this->display('public+getpassword');
+	}
+
+	public function password_email(){
+		$this->check_seccode(true);
+
+		$sEmail=trim(G::getGpc('user_email','P'));
+		if(empty($sEmail)){
+			$this->E(Dyhb::L('Email地址不能为空','Controller/Public'));
+		}
+
+		Check::RUN();
+		if(!Check::C($sEmail,'email')){
+			$this->E(Dyhb::L('Email格式不正确','Controller/Public'));
+		}
+
+		$oUser=UserModel::F('user_email=?',$sEmail)->getOne();
+		if(empty($oUser->user_id)){
+			$this->E(Dyhb::L('Email账号不存在','Controller/Public'));
+		}
+		if($oUser->user_status==0){
+			$this->E(Dyhb::L('该账户已经被禁止','Controller/Public'));
+		}
+
+		$sTemppassword=md5(G::randString(32));
+		$oUser->user_temppassword=$sTemppassword;
+		$oUser->save(0,'update');
+		if($oUser->isError()){
+			$this->E($oUser->getErrorMessage());
+		}
+
+		$sGetPasswordUrl=Dyhb::U('home://public/reset_password?email='.urlencode($sEmail).'&hash='.urlencode(str_replace('/',md5('/'),G::authcode($sTemppassword,false,null,3600))));
+
+		$oMailModel=Dyhb::instance('MailModel');
+		$oMailConnect=$oMailModel->getMailConnect();
+
+		$sEmailSubject=$GLOBALS['_option_']['site_name'].Dyhb::L('会员密码找回','Controller/Public');
+		$sNlbr=$oMailConnect->getIsHtml()===true?'<br/>':"\r\n";
+		$sEmailContent=Dyhb::L('你的登录信息','Controller/Public').':'.$sNlbr;
+		$sEmailContent.='Email:'.$sEmail.$sNlbr;
+		$sEmailContent.=Dyhb::L('重置密码链接','Controller/Public').':'.$sNlbr;
+		$sEmailContent.="<a href=\"{$sGetPasswordUrl}\">{$sGetPasswordUrl}</a>";
+
+		$oMailConnect->setEmailTo($sEmail);
+		$oMailConnect->setEmailSubject($sEmailSubject);
+		$oMailConnect->setEmailMessage($sEmailContent);
+		$oMailConnect->send();
+		if($oMailConnect->isError()){
+			$this->E($oMailConnect->getErrorMessage());
+		}
+
+		$this->S(Dyhb::L('邮件已发送到你指定的邮箱','Controller/Public'));
+	}
+
+	public function reset_password(){
+		$sEmail=trim(G::getGpc('email','G'));
+		$sHash=trim(G::getGpc('hash','G'));
+
+		if(empty($sHash)){
+			$this->U('home://public/get_password');
+		}
+		$sHash=str_replace(md5('/'),'/',$sHash);
+		$sHash=G::authcode($sHash);
+		if(empty($sHash)){
+			$this->assign('__JumpUrl__',Dyhb::U('home://public/get_password'));
+			$this->E(Dyhb::L('找回密码链接已过期','Controller/Public'));
+		}
+
+		$oUser=UserModel::F('user_email=? AND user_temppassword=?',$sEmail,$sHash)->getOne();
+		if(empty($oUser->user_id)){
+			$this->assign('__JumpUrl__',Dyhb::U('home://public/get_password'));
+			$this->E(Dyhb::L('找回密码链接已过期','Controller/Public'));
+		}
+
+		$this->assign('sEmail',$sEmail);
+
+		$this->display('public+resetpassword');
+	}
+
+	public function change_pass(){
+		$this->check_seccode(true);
+
+		$sPassword=trim(G::getGpc('user_password','P'));
+		$sNewPassword=trim(G::getGpc('new_password','P'));
+		$sEmail=trim(G::getGpc('user_email','P'));
+		
+		$oUser=UserModel::F('user_email=?',$sEmail)->getOne();
+		if(empty($oUser->user_id)){
+			$this->E(Dyhb::L('Email账号不存在','Controller/Public'));
+		}
+		if($oUser->user_status==0){
+			$this->E(Dyhb::L('该账户已经被禁止','Controller/Public'));
+		}
+
+		$oUserModel=Dyhb::instance('UserModel');
+		$oUserModel->changePassword($sPassword,$sNewPassword,'',true,$oUser->toArray(),true);
+		if($oUserModel->isError()){
+			$this->E($oUserModel->getErrorMessage());
+		}else{
+			$oUser->user_temppassword='';
+			$oUser->save(0,'update');
+			if($oUser->isError()){
+				$this->E($oUser->getErrorMessage());
+			}
+
+			$this->S(Dyhb::L('密码修改成功，你需要重新登录','Controller/Public'));
+		}
+	}
+
 	public function logout(){
 		if(UserModel::M()->isLogin()){
 			$arrUserData=$GLOBALS['___login___'];
