325512d7ee090a39377ca3c090dbe47815dffe97
weiyongming <kevin394578420@gmail.com>
2012-08-26 12:12:26 +0800

    申诉审核功能完善

--- upload/admin/App/Class/Controller/AppealController.class.php
+++ upload/admin/App/Class/Controller/AppealController.class.php
@@ -23,32 +23,89 @@ class AppealController extends InitController{
 
 		if(!empty($nAppealId)){
 			$oAppeal=AppealModel::F('appeal_id=?',$nAppealId)->getOne();
-			$oAppeal->appeal_progress=1;
-			$oAppeal->save(0,'update');
-			if(!$oAppeal->isError()){
+			if($oAppeal->appeal_progress==0){
+				$oAppeal->appeal_progress=1;
+				$oAppeal->save(0,'update');
+				if(!$oAppeal->isError()){
 				$this->assign('oAppeal',$oAppeal);
 				$this->display();
-			}else{
+				}else{
 				$this->E('无法保存用户信息');
+				}
+			}else{
+				$this->assign('oAppeal',$oAppeal);
+				$this->display();
 			}
 		}else{
-			$this->E('无法获取该用户的申诉信息');
+			$this->E('无法获取用户的申诉信息');
 		}
 	}
+
 	public function pass(){
 		$nAppealId=intval(G::getGpc('id','G'));
+
 		if(!empty($nAppealId)){
 			$oAppeal=AppealModel::F('appeal_id=?',$nAppealId)->getOne();
 			$oAppeal->appeal_progress=2;
 			$oAppeal->save(0,'update');
 			if(!$oAppeal->isError()){
-				$this->assign('oAppeal',$oAppeal);
+				$sEmail=$oAppeal->appeal_email;
+				$oUser=UserModel::F('user_id=?',$oAppeal->user_id)->getOne();
+				$sTemppassword=md5(G::randString(32));
+				$oUser->user_temppassword=$sTemppassword;
+				$oUser->save(0,'update');
+				if($oUser->isError()){
+					$this->E($oUser->getErrorMessage());
+				}
+				
+				$sGetPasswordUrl=$GLOBALS['_option_']['site_url'].'/index.php?c=getpassword&a=reset&email='.urlencode($sEmail).'&appeal=1'.'&hash='.urlencode(G::authcode($sTemppassword,false,null,$GLOBALS['_option_']['getpassword_expired']));
+
+				$oMailModel=Dyhb::instance('MailModel');
+				$oMailConnect=$oMailModel->getMailConnect();
+
+				$sEmailSubject=$GLOBALS['_option_']['site_name'].'会员申诉密码重置';
+				$sNlbr=$oMailConnect->getIsHtml()===true?'<br/>':"\r\n";
+				$sEmailContent='';
+				$sEmailContent.='重置密码链接'.':'.$sNlbr;
+				$sEmailContent.="<a href=\"{$sGetPasswordUrl}\">{$sGetPasswordUrl}</a>".$sNlbr.$sNlbr;
+				$sEmailContent.="-----------------------------------------------------".$sNlbr;
+				$sEmailContent.='这是系统用于重置密码的邮件，请勿回复'.$sNlbr;
+				$sEmailContent.='链接过期时间'.$GLOBALS['_option_']['getpassword_expired'].
+					'秒'.$sNlbr;
+
+				$oMailConnect->setEmailTo($sEmail);
+				$oMailConnect->setEmailSubject($sEmailSubject);
+				$oMailConnect->setEmailMessage($sEmailContent);
+				$oMailConnect->send();
+				if($oMailConnect->isError()){
+					$this->E($oMailConnect->getErrorMessage());
+				}
 				$this->S('审核通过');
 			}else{
-				$this->E('无法保存用户信息');
+				$this->E('审核失败');
 			}
 		}else{
-			$this->E('无法获取该用户的申诉信息');
+			$this->E('无法获取用户的申诉信息');
+		}
+	}
+
+	public function reject(){
+		$nAppealId=intval(G::getGpc('id','G'));
+		$sAppealReason=trim(G::getGpc('appeal_reason','P'));
+
+		if(!empty($nAppealId)){
+			$oAppeal=AppealModel::F('appeal_id=?',$nAppealId)->getOne();
+			$oAppeal->appeal_progress=3;
+			$oAppeal->appeal_reason=$sAppealReason;
+			$oAppeal->save(0,'update');
+			if(!$oAppeal->isError()){
+				$this->assign('__JumpUrl__',Dyhb::U('Appeal/index'));
+				$this->S('审核驳回');
+			}else{
+				$this->E('驳回失败');
+			}
+		}else{
+			$this->E('无法获取用户的申诉信息');
 		}
 	}
 }
--- upload/app/home/App/Class/Controller/UserappealController.class.php
+++ upload/app/home/App/Class/Controller/UserappealController.class.php
@@ -382,6 +382,49 @@ class UserappealController extends InitController{
 		
 		$this->display('userappeal+scheduleresult');
 	}
+	
+	public function retrieve(){
+		$nAppealId=intval(G::getGpc('id','G'));
+
+		if(!empty($nAppealId)){
+			$oAppeal=AppealModel::F('appeal_id=?',$nAppealId)->getOne();
+
+			$sEmail=$oAppeal->appeal_email;
+			$oUser=UserModel::F('user_id=?',$oAppeal->user_id)->getOne();
+			$sTemppassword=md5(G::randString(32));
+			$oUser->user_temppassword=$sTemppassword;
+			$oUser->save(0,'update');
+			if($oUser->isError()){
+				$this->E($oUser->getErrorMessage());
+			}
+				
+			$sGetPasswordUrl=$GLOBALS['_option_']['site_url'].'/index.php?c=getpassword&a=reset&email='.urlencode($sEmail).'&appeal=1'.'&hash='.urlencode(G::authcode($sTemppassword,false,null,$GLOBALS['_option_']['getpassword_expired']));
+
+			$oMailModel=Dyhb::instance('MailModel');
+			$oMailConnect=$oMailModel->getMailConnect();
+
+			$sEmailSubject=$GLOBALS['_option_']['site_name'].'会员申诉密码重置';
+			$sNlbr=$oMailConnect->getIsHtml()===true?'<br/>':"\r\n";
+			$sEmailContent='';
+			$sEmailContent.='重置密码链接'.':'.$sNlbr;
+			$sEmailContent.="<a href=\"{$sGetPasswordUrl}\">{$sGetPasswordUrl}</a>".$sNlbr.$sNlbr;
+			$sEmailContent.="-----------------------------------------------------".$sNlbr;
+			$sEmailContent.='这是系统用于重置密码的邮件，请勿回复'.$sNlbr;
+			$sEmailContent.='链接过期时间'.$GLOBALS['_option_']['getpassword_expired'].
+				'秒'.$sNlbr;
+
+			$oMailConnect->setEmailTo($sEmail);
+			$oMailConnect->setEmailSubject($sEmailSubject);
+			$oMailConnect->setEmailMessage($sEmailContent);
+			$oMailConnect->send();
+			if($oMailConnect->isError()){
+				$this->E($oMailConnect->getErrorMessage());
+			}
+			$this->S('发送成功,请注意查收');
+		}else{
+			$this->E('读取数据失败');
+		}
+	}
 
 	public function schedule_progress($nProgress){
 		if($nProgress==0){
