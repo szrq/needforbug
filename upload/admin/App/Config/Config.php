5d90ea32912cddb8d8931de351acfe969ea13ef9
doyouhaobaby liu <6357505556@qq.com>
2012-08-28 03:12:48 +0800

    提交配置文件

--- upload/admin/App/Config/Config.php
+++ upload/admin/App/Config/Config.php
@@ -6,7 +6,6 @@
 
 $arrAppConfigs=array(
 	//'START_ROUTER'=>TRUE,// 是否开启URL路由
-	'URL_MODEL'=>0, // URLMODE,
 	'PHP_OFF'=>TRUE,
 	'CACHE_LIFE_TIME'=>8640000,
 	'TMPL_MODULE_ACTION_DEPR'=>'/',
@@ -19,5 +18,6 @@ $arrGlobalConfig['SHOW_GZIP_STATUS']=FALSE;
 $arrAppConfigs['TPL_DIR']=$arrGlobalConfig['ADMIN_TPL_DIR'];
 unset($arrGlobalConfig['ADMIN_TPL_DIR']);
 $arrGlobalConfig['COOKIE_LANG_TEMPLATE_INCLUDE_APPNAME']=TRUE;
+$arrGlobalConfig['URL_MODEL']=0;
 
 return array_merge($arrAppConfigs,$arrGlobalConfig);
