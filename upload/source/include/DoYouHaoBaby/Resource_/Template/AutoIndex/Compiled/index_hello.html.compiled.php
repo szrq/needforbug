39e97cc4ddd55cd23a6e3a5d47c20378cf05a34e
doyouhaobaby liu <6357505556@qq.com>
2012-08-28 23:00:30 +0800

    提交

--- upload/source/include/DoYouHaoBaby/Resource_/Template/AutoIndex/Compiled/index_hello.html.compiled.php
+++ upload/source/include/DoYouHaoBaby/Resource_/Template/AutoIndex/Compiled/index_hello.html.compiled.php
@@ -1,4 +1,4 @@
-<?php  /* DoYouHaoBaby Framework 模板缓存文件 生成时间：2012-07-09 09:25:16  */ ?>
+<?php  /* DoYouHaoBaby Framework 模板缓存文件生成时间：2012-07-09 09:25:16  */ ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="zh-cn">
 <head>
--- upload/source/include/Tools/Lang.php
+++ upload/source/include/Tools/Lang.php
@@ -4,9 +4,9 @@
 
 /** 防止PHP页面乱码 */
 ini_set('default_charset','utf-8');
-
+exit();
 /** 导入计时器 */
-include(dirname(__FILE__).'/Tools/Lang/Timer.class.php');
+include(dirname(__FILE__).'/Tools_/Lang/Timer.class.php');
 
 /** 定义PHP运行的操作系统是否是 WINDOWS平台 */
 define('IS_WIN',DIRECTORY_SEPARATOR=='\\'?1:0);
