c781f03b27b1cd8c6c45ae9b630f08aa267bce89
doyouhaobaby liu <6357505556@qq.com>
2012-08-28 23:10:38 +0800

    框架生产应用工具语言包

---
+++ upload/source/include/Tools/Tools_/App/Lang/En-us/App.lang.php
@@ -0,0 +1,29 @@
+<?php
+/* [DoYouHaoBaby!] (C)Dianniu From 2010.
+   创建应用程序英文语言包(En-us)($)*/
+
+return array(
+'0'=>'Set the application name (%s) is invalid.',
+'1'=>'The settings directory name (%s) is invalid.',
+'2'=>'You have not set the entry file is empty.',
+'3'=>'The entrance file that you set is not a PHP file:%s.',
+'4'=>'DoYouHaoBaby application builder',
+'5'=>'Welcome use DoYouHaoBaby, this simple page can help you create a new DoYouHaoBaby application!',
+'6'=>'Application name (APP_NAME):',
+'7'=>'Application name can only letters and numbers, and the underscore.',
+'8'=>'The application Path (APP_PATH):',
+'9'=>'New application will be placed in a subdirectory of the directory, the directory name is the name of the application.',
+'10'=>'Application types:',
+'11'=>'Virtual host applications',
+'12'=>'The template for virtual host users can not modify the server settings, entrance files (index.php, admin.php, etc.) are placed in the application root directory.',
+'13'=>'Standalone server application',
+'14'=>'If it is a stand-alone server, we can deploy the application outside the Web root directory entry file in the root directory, it will enhance the security of the application.',
+'15'=>'Entrance path to the file (__ ENTER__):',
+'16'=>'If specified standalone server mode, we need to know the import documents in place.',
+'17'=>'Begin to create',
+'18'=>'Deployment instructions:',
+'19'=>'Two deployment mode, in late entrance changed files can be transformed into each other, so you do not have to worry about choosing a deployment method, and can not be converted into another way.',
+'20'=>'Defined virtual host and stand-alone server deployment just import documents defined entrance path is different, we import documents defined project path, then the project is just as import documents separation. So, we have this time, the project file to deploy to the Web root import documents placed in the web root directory, which can greatly increase the security of the site.',
+'21'=>'Of course, if we deploy the project to the outside of the Web root directory, you may need to shift resources to system-defined public directory relative to the location of the imported file. This can be seen according to the path of the damaged template, in a matter of a few seconds only.',
+'22'=>'The DoYouHaoBaby PHP Framework | BUTIFUL APP START HERE',
+);
