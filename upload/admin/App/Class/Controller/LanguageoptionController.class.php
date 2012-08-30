ddf0115d86980b0da2382ed7cd38d9b5f289af47
doyouhaobaby liu <635750556@qq.com>
2012-08-30 08:40:31 +0800

    提交

---
+++ upload/admin/App/Class/Controller/LanguageoptionController.class.php
@@ -0,0 +1,43 @@
+<?php
+/* [NeedForBug!] (C)Dianniu From 2010.
+   国际化语言包处理控制器($)*/
+
+!defined('DYHB_PATH') && exit;
+
+class LanguageoptionController extends OptionController{
+
+	public function index($sModel=null,$bDisplay=true){
+		$arrOptionData=$GLOBALS['_option_'];
+
+		$arrAdminlanguages=G::listDir(APP_PATH.'/App/Lang');
+
+		Core_Extend::loadCache('lang');
+
+		$this->assign('arrAdminlanguages',$arrAdminlanguages);
+		$this->assign('sCurrentAdminlanguage',$GLOBALS['_option_']['admin_language_name']);
+		$this->assign('arrFrontlanguages',$GLOBALS['_cache_']['lang']);
+		$this->assign('sCurrentFrontlanguage',$GLOBALS['_option_']['front_language_name']);
+		$this->assign('arrOptions',$arrOptionData);
+
+		$this->display();
+	}
+
+	public function update_option(){
+		$arrOptions=G::getGpc('options','P');
+		$sAdminlanguage=trim($arrOptions['admin_language_name']);
+		$sFrontlanguage=trim($arrOptions['front_language_name']);
+
+		// 修改后台语言包
+		OptionModel::uploadOption('admin_language_name',strtolower($sAdminlanguage));
+		Core_Extend::changeAppconfig('ADMIN_LANG_DIR',ucfirst($sAdminlanguage));
+		Dyhb::cookie('admin_language',strtolower($sAdminlanguage));
+
+		// 修改前台语言包
+		OptionModel::uploadOption('front_language_name',strtolower($sFrontlanguage));
+		Core_Extend::changeAppconfig('FRONT_LANG_DIR',ucfirst($sFrontlanguage));
+		Dyhb::cookie('language',strtolower($sFrontlanguage));
+
+		$this->S(Dyhb::L('修改国际化语言成功','Controller/Languageoption'));
+	}
+
+}
---
+++ upload/admin/App/Lang/Zh-cn/Template/Default/Languageoption.lang.php
@@ -0,0 +1,14 @@
+<?php
+/** DoYouHaoBaby Framework Lang File, Do not to modify it! */
+return array(
+'bb81f9c28ed3e69c05e7675e6051c57d'=>"国际化",
+'a472b2061ec70f0bef13399af77ee9d0'=>"防灌水基本设置",
+'af0facffa350fda369c25ab15237680f'=>"是否开启前台语言包切换",
+'7bc1cd37e4eddccf2627fd8c5e73a3f0'=>"如果选择否的话，那么用户在前台将看不到语言包切换界面以及也无法通过URL来切换语言包",
+'cff9632ddd4bd624c3cd040330766250'=>"后台语言包设置",
+'925891e60afd52be63a82a04abd24c67'=>"后台默认语言包设置，请根据你的需要来设置",
+'e5410203bfbd9da643bb44048856587f'=>"前台语言包设置",
+'dd61ce48db950b9359d2ab1fcf987a45'=>"前台默认语言包设置，前台和后台是分开的",
+)
+
+?>
