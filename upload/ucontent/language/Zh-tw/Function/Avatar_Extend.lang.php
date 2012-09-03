f727f84aada5821b475e040006433c296cd766ef
doyouhaobaby liu <635750556@qq.com>
2012-09-03 09:59:01 +0800

    Avatar_Extend.lang.php语言包增加Zh-tw版本

--- upload/ucontent/language/Zh-tw/Function/Avatar_Extend.lang.php
+++ upload/ucontent/language/Zh-tw/Function/Avatar_Extend.lang.php
@@ -2,6 +2,7 @@
 /** DoYouHaoBaby Framework Lang File, Do not to modify it! */
 return array(
 '7e2da2ad8065163136cc5d5d95a237e5'=>"上傳目錄%s不可寫",
+'9fdff224b282016eb65f1bb53ecff186'=>"無法獲取裁剪圖像",
 )
 
 ?>
