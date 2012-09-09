947f86341d58a563eb64a5db36852cfee388f63a
doyouhaobaby liu <6357505556@qq.com>
2012-09-09 19:01:19 +0800

    改变文件大小写

---
+++ upload/admin/App/Lang/Zh-cn/Template/Default/Styleoption.lang.php
@@ -0,0 +1,15 @@
+<?php
+/** DoYouHaoBaby Framework Lang File, Do not to modify it! */
+return array(
+'18876868511ea12a8db7624548356f01'=>"全局设置",
+'44fcbf7c237abcf012e93b5eef3cdb88'=>"网站Logo",
+'d1364011cba14075158f533caf30a987'=>"如果不填，则为系统默认Logo，地址为Public/images/common/logo.png",
+'d57c2985fa6b3add6abc897e52255ba6'=>"网站图片最大宽度",
+'106eaf06fed881745ebc149cc7323901'=>"网站图片宽度，如800或者80%",
+'c7337f7a7975e0513de4444324301e33'=>"是否启用前台主题切换",
+'1676f9ae79a7f1d51e5aa1841e1f725e'=>"你如果想关闭前台切换主题，请选择否",
+'d283129cce1c333507e0a2b3ff2cc7aa'=>"是否启用前台主题扩展样式切换",
+'5f2e34c5fe684549b372103e8c98c55d'=>"主题样式扩展是对前台主题的扩展，最大化简化主题的多样性",
+)
+
+?>
---
+++ upload/admin/App/Lang/Zh-tw/Template/Default/Styleoption.lang.php
@@ -0,0 +1,15 @@
+<?php
+/** DoYouHaoBaby Framework Lang File, Do not to modify it! */
+return array(
+'18876868511ea12a8db7624548356f01'=>"全局設置",
+'44fcbf7c237abcf012e93b5eef3cdb88'=>"網站Logo",
+'d1364011cba14075158f533caf30a987'=>"如果不填，則為系統默認Logo，地址為Public/images/common/logo.png",
+'d57c2985fa6b3add6abc897e52255ba6'=>"網站圖片最大寬度",
+'106eaf06fed881745ebc149cc7323901'=>"網站圖片寬度，如800或者80%",
+'c7337f7a7975e0513de4444324301e33'=>"是否啟用前台主題切換",
+'1676f9ae79a7f1d51e5aa1841e1f725e'=>"你如果想關閉前台切換主題，請選擇否",
+'d283129cce1c333507e0a2b3ff2cc7aa'=>"是否啟用前台主題擴展樣式切換",
+'5f2e34c5fe684549b372103e8c98c55d'=>"主題樣式擴展是對前台主題的擴展，最大化簡化主題的多樣性",
+)
+
+?>
