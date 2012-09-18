4c1c1baa62c659493e7795276db4278c1354648f
xiaoniuge-Dyhb <xiaoniuge@dianniu.net>
2012-09-18 16:00:49 +0800

    测试东西

---
+++ upload/app/group/App/Class/Controller/TestController.class.php
@@ -0,0 +1,17 @@
+<?php
+/* [NeedForBug!] (C)Dianniu From 2010.
+   测试控制器($)*/
+
+!defined('DYHB_PATH') && exit;
+
+class TestController extends InitController{
+
+	public function index(){
+		$this->display('test+index');
+	}
+
+	public function index2(){
+		$this->display('test+index2');
+	}
+
+}
