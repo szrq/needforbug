957202f80cdfe2ad974a3e9c606fd1126022c6db
weiyongming <kevin394578420@gmail.com>
2012-09-23 18:35:14 +0800

    基本完成主题浏览及回复列表

--- upload/app/group/App/Class/Controller/GrouptopicController.class.php
+++ upload/app/group/App/Class/Controller/GrouptopicController.class.php
@@ -42,8 +42,8 @@ class GrouptopicController extends InitController{
 	public function add_reply(){
 		//$arrOptions=$GLOBALS['_cache_']['group_option'];
 
-		$sContent=trim(G::getGpc('comment_message','P'));
-		$nId=intval(G::getGpc('tid','P'));
+		$sContent=trim(G::getGpc('comment_message'));
+		$nId=intval(G::getGpc('tid'));
 
 		if(empty($nId)){
 			$this->E('无法找到该主题');
@@ -53,24 +53,29 @@ class GrouptopicController extends InitController{
 		if(empty($oGrouptopic->grouptopic_id)){
 			$this->E('你回复的主题不存在');
 		}
-		$oGrouptopiccomment=new GrouptopicModel();
 
-		/*$nCommentPostSpace=intval($arrOptions['comment_post_space']);
-		if($nCommentPostSpace){
-		
+		//回复30秒内禁止新的回复
+		$oGrouptopiccomment=GrouptopiccommentModel::F("user_id=?",$GLOBALS['___login___']['user_id'])->order('grouptopiccomment_id DESC')->getOne();
+		if((CURRENT_TIMESTAMP-$oGrouptopiccomment->create_dateline)<30){
+			$this->E((30-(CURRENT_TIMESTAMP-$oGrouptopiccomment->create_dateline)).'秒后才能回复');
 		}
-
-		if($arrOptions['comment_repeat_check']){*/
+	
+		//一天之内禁止重复回复
 		$nCurrentTimeStamp=CURRENT_TIMESTAMP;
 		$oGrouptopiccomment=GrouptopiccommentModel::F("user_id=? AND grouptopiccomment_content=? AND {$nCurrentTimeStamp}-create_dateline<86400",$GLOBALS['___login___']['user_id'],$sContent)->getOne();
 		if(!empty($oGrouptopiccomment->user_id)){
 				$this->E('你提交的评论已经存在,24小时之内不允许出现相同的评论');
 		}
-		
+
+		$oGrouptopiccomment=new GrouptopiccommentModel();	
 		$oGrouptopiccomment->grouptopiccomment_content=$sContent;
 		$oGrouptopiccomment->grouptopic_id=$nId;
 		$oGrouptopiccomment->save(0);
-
-		$this->S('回复成功');
+		
+		$nTotalComment=GrouptopiccommentModel::F('grouptopic_id=?',$oGrouptopic->grouptopic_id)->getCounts();
+		$page=ceil($nTotalComment/5);
+		
+		$sUrl=Dyhb::U('group://grouptopic/view?id='.$oGrouptopic->grouptopic_id.'&page='.$page).'#'.($oGrouptopiccomment->grouptopiccomment_id);
+		$this->A(array('url'=>$sUrl),'回复成功',1);
 	}
 }
