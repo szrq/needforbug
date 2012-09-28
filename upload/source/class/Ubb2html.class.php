34ef97e8c6ad1034b234f6f2048c6b6a6519b1aa
xiaoniuge-Dyhb <xiaoniuge@dianniu.net>
2012-09-28 18:10:40 +0800

    附件提交

--- upload/source/class/Ubb2html.class.php
+++ upload/source/class/Ubb2html.class.php
@@ -27,11 +27,7 @@ class Ubb2html{
 		if($GLOBALS['___login___']===false){
 			$sContent=preg_replace(
 				"/\[hide\](.+?)\[\/hide\]/is",
-				self::template(
-					'隐藏内容',
-					'这部分内容只能在登入之后看到。请先 <a href="'.$this->_sRegisterurl.'">注册</a> 或者 <a href="'.$this->_sLoginurl.'">登录</a>',
-					'hidebox'
-				),
+				$this->needLogin(),
 				$sContent
 			);
 		}else{
@@ -47,28 +43,89 @@ class Ubb2html{
 		$sContent=str_replace($arrBasicUbbSearch,$arrBasicUbbReplace,$sContent);
 		
 		// URL和图像标签
-		/*$sContent=preg_replace(
+		$sContent=preg_replace(
 			"/\[url=([^\[]*)\]\[img(align=L| align=M| align=R)?(width=[0-9]+)?(height=[0-9]+)?\]\s*(\S+?)\s*\[\/img\]\[\/url\]/ise",
-			"Ubb_Extend::makeimgWithurl('\\1','\\2','\\3','\\4','\\5')",
+			"\$this->makeimgWithurl('\\1','\\2','\\3','\\4','\\5')",
 			$sContent
-		);*/
+		);
 		
 		$sContent=preg_replace(
 			"/\[img(align=L| align=M| align=R)?(width=[0-9]+)?(height=[0-9]+)?\]\s*(\S+?)\s*\[\/img\]/ise",
-			"{$this->makeImg('\\1','\\2','\\3','\\4')}",
+			"\$this->makeImg('\\1','\\2','\\3','\\4')",
 			$sContent
 		);
-/*
-			$sContent=preg_replace("/\[url=([^\[]*)\]\[upload(align=L| align=M| align=R)?(width=[0-9]+)?(height=[0-9]+)?\]\s*(\S+?)\s*\[\/upload\]\[\/url\]/ise","Ubb_Extend::makeImgWithUrl('\\1','\\2','\\3','\\4','\\5',{$nInRss},1)",$sContent);
-			$sContent=preg_replace("/\[upload(align=L| align=M| align=R)?(width=[0-9]+)?(height=[0-9]+)?\]\s*(\S+?)\s*\[\/upload\]/ise","Ubb_Extend::makeImg('\\1','\\2','\\3','\\4',{$nInRss},1)",$sContent);*/
-
-		G::dump($sContent);
+		
+		//if($GLOBALS['_option_']['ubb_content_autoaddlink']==1){
+			$sContent=preg_replace("/(?<=[^\]a-z0-9-=\"'\\/])((https?|ftp|gopher|news|telnet|rtsp|mms|callto|ed2k):\/\/|www\.)([a-z0-9\/\-_+=.~!%@?#%&;:$\\()|]+)/i","[autourl]\\1\\3[/autourl]",$sContent);
+		//}
+		
+		$arrRegUbbSearch=array(
+			"/\[size=([^\[\<]+?)\](.+?)\[\/size\]/ie",
+			"/\s*\[quote\][\n\r]*(.+?)[\n\r]*\[\/quote\]\s*/is",
+			"/\s*\[quote=(.+?)\][\n\r]*(.+?)[\n\r]*\[\/quote\]\s*/is",
+			"/\s*\[code\][\n\r]*(.+?)[\n\r]*\[\/code\]\s*/ie",
+			"/\[autourl\]([^\[]*)\[\/autourl\]/ie",
+			"/\[url\]([^\[]*)\[\/url\]/ie",
+			"/\[url=([^\[]*)\](.+?)\[\/url\]/ie",
+			"/\[email\]([^\[]*)\[\/email\]/is",
+			"/\[acronym=([^\[]*)\](.+?)\[\/acronym\]/is",
+			"/\[color=([a-zA-Z0-9#]+?)\](.+?)\[\/color\]/i",
+			"/\[font=([^\[\<:;\(\)=&#\.\+\*\/]+?)\](.+?)\[\/font\]/i",
+			"/\[p align=([^\[\<]+?)\](.+?)\[\/p\]/i",
+			"/\[b\](.+?)\[\/b\]/i",
+			"/\[i\](.+?)\[\/i\]/i",
+			"/\[u\](.+?)\[\/u\]/i",
+			"/\[blockquote\](.+?)\[\/blockquote\]/i",
+			"/\[strong\](.+?)\[\/strong\]/i",
+			"/\[strike\](.+?)\[\/strike\]/i",
+			"/\[sup\](.+?)\[\/sup\]/i",
+			"/\[sub\](.+?)\[\/sub\]/i",
+			"/\s*\[php\][\n\r]*(.+?)[\n\r]*\[\/php\]\s*/ie"
+		);
+		
+		$arrRegUbbReplace=array(
+			"\$this->makeFontsize('\\1','\\2')",
+			$this->template('引用',"\\1"),
+			$this->template("引用自 \\1","\\2"),
+			"\$this->makeCode('\\1')",
+			"\$this->makeUrl('\\1',1)",
+			"\$this->makeUrl('\\1','0')",
+			"\$this->makeUrl('\\1','0','\\2')",
+			"<a href=\"mailto:\\1\">\\1</a>",
+			"<acronym title=\"\\1\">\\2</acronym>",
+			"<span style=\"color: \\1;\">\\2</span>",
+			"<span style=\"font-family: \\1;\">\\2</span>",
+			"<p align=\"\\1\">\\2</p>",
+			"<strong>\\1</strong>",
+			"<em>\\1</em>",
+			"<u>\\1</u>",
+			"<blockquote>\\1</blockquote>",
+			"<strong>\\1</strong>",
+			"<del>\\1</del>",
+			"<sup>\\1</sup>",
+			"<sub>\\1</sub>",
+			"\$this->xhtmlHighlightString('\\1')"
+		);
+		
+		$sContent=preg_replace($arrRegUbbSearch,$arrRegUbbReplace,$sContent);
+		
+		// 解析上传附件
+		$sContent=preg_replace("/\[attachment\]\s*(\S+?)\s*\[\/attachment\]/ise","\$this->makeAttachment('\\1','{$this->_nOuter}')",$sContent);
+		
 		return $sContent;
 	}
 
-	public function makeImg($sAlignCode,$sWidthCode,$sHeightCode,$sSrc){
-		$sAlign=str_replace(' align=','',strtolower($sAlignCode));
+	public function makeimgWithurl($sUrl,$sAlignCode,$sWidthCode,$sHeightCode,$sSrc){
+		return $this->makeImg($sAlignCode,$sWidthCode,$sHeightCode,$sSrc,$sUrl);
+	}
+	
+	public function makeImg($sAlignCode,$sWidthCode,$sHeightCode,$sSrc,$sUrl=''){
+		if(empty($sUrl)){
+			$sUrl=$sSrc;
+		}
 		
+		// 对齐
+		$sAlign=str_replace(' align=','',strtolower($sAlignCode));
 		if($sAlign=='l'){
 			$sShow=' align="left"';
 		}elseif($sAlign=='r'){
@@ -77,6 +134,7 @@ class Ubb2html{
 			$sShow='';
 		}
 		
+		// 宽度&高度
 		$nWidth=str_replace(' width=','',strtolower($sWidthCode));
 		if(!empty($nWidth)){
 			$sShow.=' width="'.$nWidth.'"';
@@ -87,33 +145,124 @@ class Ubb2html{
 			$sShow.=' height="'.$nHeight.'"';
 		}
 		
-		$sCode="<a href=\"{$sSrc}\" target=\"_blank\"><img src=\"{$sSrc}\" class=\"content-insert-image\" alt=\"在新窗口浏览此图片\" title=\"在新窗口浏览此图片\" border=\"0\" {$sShow}/></a>";
+		return "<a href=\"{$sUrl}\" target=\"_blank\"><img src=\"{$sSrc}\" class=\"content-insert-image\" alt=\"在新窗口浏览此图片\" title=\"在新窗口浏览此图片\" border=\"0\" {$sShow}/></a>";
+	}
+	
+	public function makeFontsize($nSize,$sWord){
+		$sWord=stripslashes($sWord);
+		$nSizeItem=array(0,8,10,12,14,18,24,36);
+		$nSize=$nSizeItem[$nSize];
 		
-		return $sCode;
+		return "<span style=\"font-size:{$nSize}px;\">{$sWord}</span>";
+	}
+	
+	public function makeCode($sStr){
+		$sStr=str_replace('[autourl]','',$sStr);
+		$sStr=str_replace('[/autourl]','',$sStr);
+		
+		return "<div class=\"ubb_code\">{$sStr}</div>";
+	}
+	
+	public function makeUrl($sUrl,$nAutolink=0,$sLinkText=''){
+		if($nAutolink==1){
+			$sGoToRealLink=$GLOBALS['_option_']['site_url'].'/urlredirect.php?go='.(substr(strtolower($sUrl),0,4)=='www.'?urlencode("http://{$sUrl}"):urlencode($sUrl));
+		}else{
+			$sGoToRealLink=substr(strtolower($sUrl),0,4)=='www.'?"http://{$sUrl}":$sUrl;
+		}
+		
+		$sUrlLink="<a href=\"{$sGoToRealLink}\" target=\"_blank\">";
+		
+		if(!empty($sLinkText)){
+			$sUrl=$sLinkText;
+		}else{
+			if($GLOBALS['_option_']['ubb_content_shorturl'] && strlen($sUrl)>$GLOBALS['_option_']['ubb_content_urlmaxlen']){
+				$nHalfMax=floor($GLOBALS['_option_']['ubb_content_urlmaxlen']/2);
+				$sUrl=substr($sUrl,0,$nHalfMax).'...'.substr($sUrl,0-$nHalfMax);
+			}
+		}
+		
+		$sUrlLink.=$sUrl.'</a>';
+		
+		return $sUrlLink;
+	}
+	
+	public function xhtmlHighlightString($sStr){
+		$sHlt=highlight_string($sStr,true);
+		if(PHP_VERSION>'5'){
+			return "<div class=\"ubb_code\" style=\"overflow: auto;\">{$sHlt}</div>";
+		}
+		
+		$sFon=str_replace(array('<font ','</font>'),array('<span ','</span>'),$sHlt);
+		$sRet=preg_replace('#color="(.*?)"#','style="color: \\1"',$sFon);
+		
+		return "<div class=\"ubb_code\" style=\"overflow: auto;\">{$sRet}</div>";
 	}
 
-	public function getAttachmentpath($Url){
-		/*if(!preg_match('/^http:\/\/[A-Za-z0-9]+\.[A-Za-z0-9]+[\/=\?%\-&_~`@[\]\':+!]*([^<>\"\"])*$/',$Url)){
-			if(!preg_match("/[^\d-.,]/",$Url)){
-				$oAttachment=AttachmentModel::F('attachment_id=?',$Url)->query();
+	public function makeAttachment($nId){
+		$oAttachment=$this->getAttachment($nId);
+		if($oAttachment===false){
+			return '';
+		}
+		
+		$sType=Core_Extend::getAttachmenttype($oAttachment);
+		
+		return call_user_func(array($this,'attachment'.ucfirst($sType)),$oAttachment,$this->_nOuter);
+	}
+	
+	public function attachmentImg($oAttachment,$nOuter=0){
+		$sImg=Core_Extend::getAttachmenturl($oAttachment);
+		
+		if($nOuter==0){
+			if($GLOBALS['_option_']['upload_loginuser_view']==1 && $GLOBALS['___login___']===FALSE){
+				return $this->needLogin();
 			}else{
-				$oAttachment=AttachmentModel::F('attachment_savename=?',$Url)->query();
-			}
-			
-			if(!empty($oAttachment['attachment_id'])){
-				return false;
-			}
-			
-			/*if($GLOBALS['_option_']['upload_ishide_reallypath']==1){
-				$sUrl=
-			}else{
+				$sTitle='<a href="'.Dyhb::U('home://file@?id='.$oAttachment['attachment_id']).'" target="_blank">'.$oAttachment['attachment_name'].'</a>';
+				$sTitle.=' | <a href="'.Dyhb::U('home://file@?id='.$oAttachment['attachment_id']).'#comments" target="_blank">评论('.$oAttachment['attachment_commentnum'].')</a>';
+				$sTitle.=' | <a href="'.Dyhb::U('home://space@?id='.$oAttachment['user_id']).'" target="_blank">'.$oAttachment['attachment_username'].'</a>';
 				
-			}*/
+				$sTitlemore='';
+				$sTitlemore.=' | 已下载('.$oAttachment['attachment_download'].')次';
+				$sTitlemore.=' | '.G::changeFileSize($oAttachment['attachment_size']);
+				$sTitlemore.=" | Upload Time:".date('Y-m-d H:i',$oAttachment['create_dateline']);
+				$sContent="<a href=\"{$sImg}\" target=\"_blank\"><img src=\"{$sImg}\" class=\"content-insert-image\" alt=\"{$oAttachment['attachment_alt']}\" title=\"在新窗口浏览此图片{$sTitlemore}\" \" border=\"0\"/></a>";
+				
+				return $this->template($sTitle,$sContent);
+			}
+		}else{
+			return "<a href=\"{$sImg}\" target=\"_blank\"><img src=\"{$sImg}\" class=\"content-insert-image\" alt=\"在新窗口浏览此图片\" title=\"在新窗口浏览此图片\" border=\"0\"/></a>";
+		}
+	}
+	
+	public function attachmentSwf($oAttachment,$nOuter=0){
+		$sImgtarget=Core_Extend::getAttachmenturl($oAttachment);
+		
+		if($nOuter==0){
 			
-			/*return $oAttachment
-		}*/
-
-		return $Url;
+		}else{
+			return "<a href=\"".Dyhb::U('home://file@?id='.$oAttachment['attachment_id'])."\" target=\"_blank\">{$oAttachment['attachment_name']}</a>";
+		}
+	}
+	
+	public function getAttachment($nId){
+		if(!preg_match("/[^\d-.,]/",$nId)){
+			$oAttachment=AttachmentModel::F('attachment_id=?',$nId)->query();
+		}else{
+			$oAttachment=AttachmentModel::F('attachment_savename=?',$nId)->query();
+		}
+		
+		if(empty($oAttachment['attachment_id'])){
+			return false;
+		}
+		
+		return $oAttachment;
+	}
+	
+	protected function needLogin(){
+		return $this->template(
+					'隐藏内容',
+					'这部分内容只能在登入之后看到。请先 <a href="'.$this->_sRegisterurl.'">注册</a> 或者 <a href="'.$this->_sLoginurl.'">登录</a>',
+					'hidebox'
+				);
 	}
 
 	protected function template($sTitle,$sContent,$sId='ubb_box'){
