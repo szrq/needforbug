30f05586e8495699a4f4ff63de0611d45929a89c
doyouhaobaby liu <6357505556@qq.com>
2012-10-02 11:42:07 +0800

    删除缓存

--- upload/data/~runtime/home/Cache/Default/attachment_dialogadd.html.compiled.php
+++
@@ -1,2 +0,0 @@
-<?php  /* DoYouHaoBaby Framework 模板缓存文件生成时间：2012-10-02 03:32:35  */ ?>
-<?php $this->includeChildTemplate(Core_Extend::template('cheader'));?><?php $this->includeChildTemplate(TEMPLATE_PATH.'/attachment_dialogmenu.html');?><div class="span12"><div class="dialog_add"><?php $this->includeChildTemplate(TEMPLATE_PATH.'/attachment_add_.html');?></div></div><?php $this->includeChildTemplate(Core_Extend::template('cfooter'));?>
