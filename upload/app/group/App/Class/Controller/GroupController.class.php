aed34a9ef60cff51c705c01d670a1950d49b6717
weiyongming <kevin394578420@gmail.com>
2012-10-03 17:21:06 +0800

    小组帖子

--- upload/app/group/App/Class/Controller/GroupController.class.php
+++ upload/app/group/App/Class/Controller/GroupController.class.php
@@ -14,6 +14,18 @@ class GroupController extends InitController{
 			$this->E('小组不存在或者还在审核中');
 		}
 		
+		$arrWhere=array();
+		$nEverynum=4;
+		$arrWhere['grouptopic_status']=1;
+		$arrWhere['grouptopic_isaudit']=1;	
+		$arrWhere['group_id']=$oGroup->group_id;
+		$nTotalComment=GrouptopicModel::F()->where($arrWhere)->all()->getCounts();
+		$oPage=Page::RUN($nTotalComment,$nEverynum,G::getGpc('page','G'));
+		$arrGrouptopics=GrouptopicModel::F()->where($arrWhere)->limit($oPage->returnPageStart(),$nEverynum)->getAll();
+		$this->assign('arrGrouptopics',$arrGrouptopics);
+		$this->assign('nEverynum',$nEverynum);
+		$this->assign('sPageNavbar',$oPage->P('pagination','li','active'));
+
 		$this->assign('oGroup',$oGroup);
 		
 		$this->display('group+show');
