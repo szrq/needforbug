5a1ecc7415e540754e0ca23263382ca6ebbd6731
xiaoniuge-Dyhb <xiaoniuge@dianniu.net>
2012-10-22 18:05:06 +0800

    准备回去了

--- upload/app/group/App/Class/Controller/GrouptopicadminController.class.php
+++ upload/app/group/App/Class/Controller/GrouptopicadminController.class.php
@@ -6,12 +6,44 @@
 
 class GrouptopicadminController extends InitController{
 
-	public function moderate(){
-		$nGroupid=trim(G::getGpc('groupid','G'));
-		$arrGrouptopics=G::getGpc('moderate','G');
-		$nGrouptopicid=intval(G::getGpc('from','G'));
+	public function deletetopic_dialog(){
+		$nGroupid=intval(G::getGpc('groupid','G'));
+		$arrGrouptopics=G::getGpc('dataids','G');
 		
-		$this->display('grouptopicadmin+deletetopic');
+		if(empty($nGroupid)){
+			$this->E('没有待操作的小组');
+		}
+
+		$oGroup=GroupModel::F('group_id=?',$nGroupid)->getOne();
+		if(empty($oGroup['group_id'])){
+			$this->E('没有找到指定的小组');
+		}
+		
+		if(empty($arrGrouptopics)){
+			$this->E('没有待操作的帖子');
+		}
+
+		$sGrouptopics=implode(',',$arrGrouptopics);
+
+		$this->assign('nGroupid',$nGroupid);
+		$this->assign('sGrouptopics',$sGrouptopics);
+		
+		$this->display('grouptopicadmin+deletetopicdialog');
+	}
+
+	public function deletetopic(){
+		$nGroupid=intval(G::getGpc('group_id','P'));
+		$sGrouptopics=G::getGpc('grouptopics','G');
+
+		$arrGrouptopics=implode(',',$sGrouptopics);
+		if(is_array($arrGrouptopics)){
+			foreach($arrGrouptopics as $nGrouptopic){
+				$oGrouptopicMeta=GrouptopicModel::M();
+				$oGrouptopicMeta->deleteWhere(array('grouptopic_id'=>$nGrouptopic));
+			}
+		}
+
+		$this->S('删除主题成功');
 	}
 
 }
--- upload/app/group/Static/Js/moderator.js
+++ upload/app/group/Static/Js/moderator.js
@@ -1,18 +1,24 @@
 /* [NeedForBug!] (C)Dianniu From 2010.
    Group应用帖子操作($)*/
 
-/** 操作 */
-function modTopics(nOptgroup,sOperation){
-	var sOperation=!sOperation?'':sOperation;
-	
-	$("#modActions").attr("action",D.U('group://grouptopicadmin/moderate?groupid='+nGroupid+'&moderate[]='+nGrouptopicid+(nOptgroup!=3 && nOptgroup!=2?'&from='+nGrouptopicid:'')));
-	document.getElementById('modActions').optgroup.value=nOptgroup;
-	document.getElementById('modActions').operation.value=sOperation;
-
-	var sHtml = $.ajax({
-		url: D.U('group://grouptopicadmin/moderate?groupid='+nGroupid+'&moderate[]='+nGrouptopicid+(nOptgroup!=3 && nOptgroup!=2?'&from='+nGrouptopicid:'')),
+function modTopicdelete(){
+	var sHtml=$.ajax({
+		url: D.U('group://grouptopicadmin/deletetopic_dialog?groupid='+nGroupid+'&dataids[]='+nGrouptopicid),
 		async: false
 	}).responseText;
 
-	oEditNewmodtopics=needforbugAlert(sHtml,'你选择了一篇帖子','','','',400,100);
+	oEditNewmodtopics=needforbugAlert(sHtml,'你选择了一篇帖子','',modTopicdeletetopic,'',400,100);
+}
+
+function modTopicdeletetopic(){
+	Dyhb.AjaxSubmit('moderateform',D.U('group://grouptopicadmin/deletetopic'),'result',deletetopicComplete);
+	return false;
+}
+
+function deletetopicComplete(){
+	return true;
+}
+
+function replaceIdcontent(id,content){
+	$('#'+id).val(content);
 }
