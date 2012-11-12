d1ddd54d1a7fb699532646a361f63b24bac1be92
xiaoniuge <635750556@qq.com>
2012-11-12 12:10:10 +0800

    开始前台商品购物过程开发

---
+++ upload/app/shop/App/Class/Controller/ShopgoodsController.class.php
@@ -0,0 +1,13 @@
+<?php
+/* [NeedForBug!] (C)Dianniu From 2010.
+   商城商品控制器($)*/
+
+!defined('DYHB_PATH') && exit;
+
+class ShopgoodsController extends InitController{
+
+	public function view(){
+		$this->display('shopgoods+view');
+	}
+
+}
