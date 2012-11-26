c8208af778d15daa5b3b4fd712edd81010e16dea
刘祥敏 <xiaoniuge@dianniu.net>
2012-11-26 17:47:14 +0800

    提交代码

---
+++ upload/app/shop/App/Class/Controller/ShopeduController.class.php
@@ -0,0 +1,13 @@
+<?php
+/* [NeedForBug!] (C)Dianniu From 2010.
+   商城教育控制器($)*/
+
+!defined('DYHB_PATH') && exit;
+
+class ShopeduController extends InitController{
+
+	public function index(){
+		$this->display('shopedu+index');
+	}
+
+}
---
+++ upload/app/shop/App/Class/Controller/ShopshowController.class.php
@@ -0,0 +1,13 @@
+<?php
+/* [NeedForBug!] (C)Dianniu From 2010.
+   商城在线展览控制器($)*/
+
+!defined('DYHB_PATH') && exit;
+
+class ShopshowController extends InitController{
+
+	public function index(){
+		$this->display('shopshow+index');
+	}
+
+}
