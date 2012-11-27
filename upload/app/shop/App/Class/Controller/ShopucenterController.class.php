df4e5cb7fcfebc87db7888e2107b6bba600b3fa2
刘祥敏 <xiaoniuge@dianniu.net>
2012-11-27 11:58:35 +0800

    提交代码

---
+++ upload/app/shop/App/Class/Controller/ShopucenterController.class.php
@@ -0,0 +1,13 @@
+<?php
+/* [NeedForBug!] (C)Dianniu From 2010.
+   商城个人中心控制器($)*/
+
+!defined('DYHB_PATH') && exit;
+
+class ShopucenterController extends InitController{
+
+	public function index(){
+		$this->display('shopucenter+index');
+	}
+
+}
