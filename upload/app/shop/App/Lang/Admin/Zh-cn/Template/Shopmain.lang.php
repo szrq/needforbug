dfe3bcb610e877fcfc0408e94792234c278d4191
liuxiangmin <xiaoniuge@dianniu.net>
2012-12-03 17:00:06 +0800

    提交后台界面

---
+++ upload/app/shop/App/Lang/Admin/Zh-cn/Template/Shopmain.lang.php
@@ -0,0 +1,14 @@
+<?php
+/** DoYouHaoBaby Framework Lang File, Do not to modify it! */
+return array(
+'a54d9b0788202fbe9b3899632b118d9a'=>"商城入口",
+'ca03bb7546eaf8059a490d2cf89fb3e6'=>"商城设置",
+'5782b6eac4b724022c1c68499a6e342d'=>"商品管理",
+'f435708a59edb82e6f368cb29895ee38'=>"文章管理",
+'afcd116e3e5de404c18d9ff4a2f0b3af'=>"订单管理",
+'fbfeda2c01f49a7aae34b788cc73eea2'=>"物流管理",
+'102b3746b3ff7854f3c2408dccd61339'=>"营销推广",
+'41827176dd82c215dc0c2bc57cfb5761'=>"统计工具",
+)
+
+?>
